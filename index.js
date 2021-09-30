4b1fd7a7c6d222e4c25346a21d3587a341239c76
Zix0722 <90633809+Zix0722@users.noreply.github.com>
2021-09-30 17:23:01 -0400

    Update index.js

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     document.querySelectorAll(".form__input").forEach(inputElement => {
         inputElement.addEventListener("blur", e => {
-            if (e.target.id ==="signupUsername" && e.target.value.length > 0 && e.target.value.length < 6){
+            if (e.target.id ==="signupUsername" && e.target.value.length > 0 && e.target.value.length < 4){
                 setInputError(inputElement, "Username must be at least 4 characters in length");
             }
         });
